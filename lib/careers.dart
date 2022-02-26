5fc7889792a2bc147c19708510e7021142f09beb
thequickbrownfoxjumpedoverthelazydog <c25kenneth@gmail.com>
2022-02-25 20:54:01 -0800

    managed to get careers from firestore

--- lib/careers.dart
+++ lib/careers.dart
@@ -1,12 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Careers extends StatelessWidget {
-  const Careers({ Key? key }) : super(key: key);
+class Careers extends StatefulWidget {
+  Careers({ Key? key }) : super(key: key);
+
+  @override
+  State<Careers> createState() => _CareersState();
+}
+
+class _CareersState extends State<Careers> {
+  final Stream<QuerySnapshot> _careersStream = FirebaseFirestore.instance.collection('careers').snapshots(); 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('All Jobs and Internships'),),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _careersStream,
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if(snapshot.hasError) {
+              return Text('Something went wrong'); 
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text('Loading'); 
+            } else {
+              return SizedBox(
+                height: 90,
+                child: Card(
+                  child: ListView(
+                    children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                      Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                      return ListTile(
+                        title: Text(data['companyName']),
+                        subtitle: Text(data['positionName']),
+                        onTap: (){
+                          showBottomSheet(context: context, builder: (context) {
+                            return Wrap(
+                              children: [
+                                ListTile(title: Text(data['companyName'] + ': ' + data['positionDescription']), subtitle: Text(data['positionLink']),),
+                              ],
+                            ); 
+                          });
+                        }
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ); 
+            }
+        },  
+      ),
     );
   }
 }
