40abe63814e09e54d25618852086322865d2a32e
thequickbrownfoxjumpedoverthelazydog <c25kenneth@gmail.com>
2022-02-25 19:05:21 -0800

    Added firebase auth functions

---
+++ lib/fireflutter.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+final FirebaseAuth auth = FirebaseAuth.instance; 
+final User? user = auth.currentUser; 
+final uid = user?.uid;  
+
+Future<bool> canRegister(String userEmail, userPassword) async {
+  try {
+    FirebaseAuth.instance.createUserWithEmailAndPassword(email: userEmail, password: userPassword);
+    return true; 
+  } catch(e) {
+    return false; 
+  }
+    
+}
+
+Future signOut() async {
+  try {
+    await FirebaseAuth.instance.signOut(); 
+    return true; 
+  } catch (e) {
+    return false; 
+  }
+}
+
+Future<bool> canSignIn(String userEmail, userPassword) async {
+  try {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(email: userEmail, password: userPassword); 
+    return true; 
+  } catch (e) {
+    return false; 
+  }
+}
