abdfe6c07c53de85d28e00ef57cfb4e12d24a135
thequickbrownfoxjumpedoverthelazydog <c25kenneth@gmail.com>
2022-02-25 19:55:12 -0800

    Added empty files for different screens

---
+++ lib/scholarships.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Scholarships extends StatelessWidget {
+  const Scholarships({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('All Scholarships'),
+      ),
+    );
+  }
+}
